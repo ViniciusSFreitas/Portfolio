9bf2408ad4bb1e6c3add5a974e87cc3ef576e299
Vinicius Freitas <viniciusfreitas566@gmail.com>
2023-12-18 19:36:28 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+function hoverProject(element) {
+    element.style.boxShadow = '0 6px 12px rgba(0, 0, 0, 0.2)';
+}
+
+function unhoverProject(element) {
+    element.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+}
